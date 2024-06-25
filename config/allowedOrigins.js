a6dd758ff494ff943eb7b6e55aa1245235d4667d
yash070504 <yashiitg376@gmail.com>
2024-06-25 17:11:16 +0530

    ready

--- config/allowedOrigins.js
+++ config/allowedOrigins.js
@@ -1,7 +1,11 @@
 const allowedOrigins = [
    'https://localhost:3500',
     'http://localhost:5173',
-    'https://complaint-portal-cg5e.vercel.app/'
+    'https://complaint-portal-cg5e.vercel.app/' ,
+    'https://complaint-portal-yash070504-fo26znj06.vercel.app',
+    'https://complaint-portal-yash070504-fo26znj06.vercel.app/',
+    'https://complaint-portal-yash070504.vercel.app',
+    'https://complaint-portal-yash070504.vercel.app/'
 ]
 
 module.exports = allowedOrigins ;
